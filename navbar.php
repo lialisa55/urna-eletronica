ffb3454bdd594ac5eebe9a37749e6b27f3b9e278
lialisa55 <nathalia.mota@etec.sp.gov.br>
2023-04-30 14:15:44 -0300

    resolvendo conflitos de merge

---
+++ navbar.php
@@ -0,0 +1,60 @@
+<!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Urna Eletrônica</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="cadastro.php">Cadastro</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="login.php">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Votar</a>
+              </li>
+          </ul>
+        </div>
+      </nav> -->
+
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="index.html">Urna Eletrônica</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <div class="mx-auto"></div>
+          <ul class="navbar-nav">
+          <li class="nav-item">
+              <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="cadastro.php">Cadastro</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="login.php">Entrar</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="sair.php">Sair</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Votar</a>
+              </li>
+          </ul>
+        </div>
+      </div>
+    </nav>

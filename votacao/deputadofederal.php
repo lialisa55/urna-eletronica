54639cf45382e6816951bab4c3a32f0e9768fe91
unknown <nathalia.mota@etec.sp.gov.br>
2023-05-05 19:06:52 -0300

    completando configurações de grid no layout de votação

--- votacao/deputadofederal.php
+++ votacao/deputadofederal.php
@@ -15,31 +15,39 @@
 ?>
 <div class="container fluid">
   <div class="row">
-    <form action="valida-cadastro.php" method="GET" class="row-3 mt-5">    
+    <form action="valida-cadastro.php" method="GET" class="col-3 mt-5 mx-auto">    
       <div class="row">
-        <div class="col-sm-3 my-1">
+        <div class="col my-1">
           <label for="inputZip" class="form-label">Código</label>
           <input name="numero" type="text" class="form-control">
         </div>
-        <div class="col-sm-3 my-1">
+        <div class="col my-1">
           <button type="search" class="btn btn-primary">Pesquisar</button>
         </div>
-        </div>
-
-        <div class="col-sm-3 my-1">
+      </div>
+      <div class="row">
+        <div class="col my-1">
             <label for="nome-candidato" class="form-label">Nome</label>
             <input name="nome-candidato" type="text" class="form-control">
         </div>
-        <div class="col-sm-3 my-1">
+      </div>
+      <div class="row">
+        <div class="col my-1">
             <label for="inputZip" class="form-label">Partido</label>
             <input name="numero" type="text" class="form-control">
         </div>
+      </div>
+      <div class="row">
         <div class="col-10">
             <button type="submit" class="btn btn-primary my-3">Registrar</button>
         </div>
+      </div>
     </form>
-    <div class='row-3 mt-5'>
-      <img src="coelho-de-estimacao-2.jpg" class="img-thumbnail" width="300px"/>
+    <div class='col-3 mt-5 mx-auto'>
+      <img src="coelho-de-estimacao-2.jpg" class="img-thumbnail" width="250px"/>
+    </div>
+    <div class='col-3 mt-5 mx-auto'>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ullamcorper massa vitae nulla bibendum vulputate. Maecenas porta dignissim mauris. Maecenas vitae sapien varius nibh porttitor elementum. Phasellus porta eu odio sit amet pellentesque. In maximus at nunc sit amet efficitur. Sed vitae tincidunt nisl, et convallis elit. Mauris id lobortis sem, sed tincidunt tortor. Nam ullamcorper vehicula sodales.</p>
     </div>
   </div>
 </div>

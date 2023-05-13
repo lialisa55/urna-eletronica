d212e17d8f4446beb2cdaa2f2f3464115ab7093e
lialisa55 <nathalia.mota@etec.sp.gov.br>
2023-05-12 23:11:11 -0300

    limpando código

--- votacao/deputadoestadual.php
+++ votacao/deputadoestadual.php
@@ -13,7 +13,6 @@
     include ('../limitacao.php');
     include ('navbarvotacao.php');
 ?>
-<<<<<<< HEAD
 <div class="container fluid">
   <div class="row">
     <form action="" method="POST" class="col-3 mt-5 mx-auto">
@@ -90,31 +89,3 @@ if(isset($_POST['nome']) && isset($_POST['partido']) && isset($_POST['codigo']))
   }
   
   ?>
-
-<form action="valida-cadastro.php" method="GET" class="container mt-5">    
-    <div class="col-sm-3 my-1">
-      <label for="inputZip" class="form-label">Código</label>
-      <input name="numero" type="text" class="form-control">
-    </div>
-    <div class="col-10">
-        <button type="search" class="btn btn-primary my-3">Pesquisar</button>
-    </div>
-    <div class="col-sm-3 my-1">
-        <label for="nome-candidato" class="form-label">Nome</label>
-        <input name="nome-candidato" type="text" class="form-control">
-    </div>
-    <div class="col-sm-3 my-1">
-        <label for="inputZip" class="form-label">Partido</label>
-        <input name="numero" type="text" class="form-control">
-    </div>
-    <div class="col-10">
-        <button type="submit" class="btn btn-primary my-3">Registrar</button>
-    </div>
-</form>
-
-<div class="container mt-5">
-  <img source="coelho-de-estimacao-2.jpg" class="img-fluid"/>
-  <?php
-
-  ?>
-</div>
